4ef2b0feeb47d89dc9bc425ddbc0ccaf330559db
xtobal101 <96153850+xtobal101@users.noreply.github.com>
2023-03-31 16:29:50 -0600

    A basic hello world program using the Fyne gui framework
    --Intalled Fyne using:
    
         go get fyne.io/fyne/v2@latest
    
    --Build the main.exe without the poping a console using:
    
         go build -ldflags -H=windowsgui main.go
    
    --Note: Used the following to fix the building isues:
    
         go mod tidy

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"fmt"	
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
-
+	
 func main() {
-	fmt.Println("hello world")
+		fmt.Println("hello world")
+
+		a := app.New()
+		w := a.NewWindow("Hello World")
+	
+		w.SetContent(widget.NewLabel("Hello World!"))
+		w.ShowAndRun()
 }
+
+
